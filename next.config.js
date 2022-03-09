51df32b2260730a218c635ad758e9a4b07b33396
Andre Putera Pratama <andreputerap@gmail.com>
2022-03-09 17:07:39 +0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+    reactStrictMode: true,
+    productionBrowserSourceMaps: false,
 }
 
 module.exports = nextConfig

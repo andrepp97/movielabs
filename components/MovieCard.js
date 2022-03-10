3a203179fcbeed8cae5d67283aa04c9adb801feb
Andre Putera Pratama <andreputerap@gmail.com>
2022-03-11 00:19:22 +0700

    Update MovieCard.js

--- components/MovieCard.js
+++ components/MovieCard.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import Link from 'next/link'
 import { motion } from 'framer-motion'
 import styles from '../styles/MovieCard.module.css'
 
@@ -6,26 +6,28 @@ const imgURL = 'https://image.tmdb.org/t/p/w500'
 
 const MovieCard = ({ data }) => {
     return (
-        <motion.div
-            layout
-            animate={{ opacity: 1 }}
-            initial={{ opacity: 0 }}
-            exit={{ opacity: 0 }}
-            whileTap={{ scale: 1 }}
-            whileHover={{ scale: 1.02, transition: { duration: .2 } }}
-            className={styles.movieCard}
-        >
-            <img
-                alt={data.title}
-                src={imgURL + data.poster_path}
-            />
-            <p className={styles.movieYear}>
-                ({data.release_date.split('-')[0]})
-            </p>
-            <p className={styles.movieTitle}>
-                {data.title}
-            </p>
-        </motion.div>
+        <Link href={'/movies/' + data.id}>
+            <motion.div
+                layout
+                animate={{ opacity: 1 }}
+                initial={{ opacity: 0 }}
+                exit={{ opacity: 0 }}
+                whileTap={{ scale: 1 }}
+                whileHover={{ scale: 1.02, transition: { duration: .2 } }}
+                className={styles.movieCard}
+            >
+                <img
+                    alt={data.title}
+                    src={imgURL + data.poster_path}
+                />
+                <p className={styles.movieYear}>
+                    ({data.release_date.split('-')[0]})
+                </p>
+                <p className={styles.movieTitle}>
+                    {data.title}
+                </p>
+            </motion.div>
+        </Link>
     );
 };
 

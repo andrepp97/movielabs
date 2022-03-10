25575e70eb9184e2f73fab898d4a82a468dfda3c
Andre Putera Pratama <andreputerap@gmail.com>
2022-03-11 00:19:14 +0700

    Create Footer.js

---
+++ components/Footer.js
@@ -0,0 +1,9 @@
+const Footer = () => {
+    return (
+        <footer>
+            &copy; Copyright 2022 <strong> | Andre Putera Pratama</strong>
+        </footer>
+    );
+}
+
+export default Footer;

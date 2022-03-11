8123d78dabc7aea9b5ddeddb2fc631a128eb6f72
Andre Putera Pratama <andreputerap@gmail.com>
2022-03-11 16:36:23 +0700

    Update [id].js

--- pages/movies/[id].js
+++ pages/movies/[id].js
@@ -2,10 +2,14 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { useRouter } from 'next/router'
 import { useState, useEffect, useCallback } from 'react'
-import { Skeleton } from '../../components'
+import { BsPlayFill, BsImages } from 'react-icons/bs'
+import { AnimatePresence } from 'framer-motion'
+import { Skeleton, Modal } from '../../components'
 import styles from '../../styles/MovieDetails.module.css'
 
 const imgURL = 'https://image.tmdb.org/t/p/w500'
+const castURL = 'https://image.tmdb.org/t/p/w138_and_h175_face'
+const backdropURL = 'https://image.tmdb.org/t/p/original'
 
 const MovieDetails = () => {
     // State & Params
@@ -13,8 +17,18 @@ const MovieDetails = () => {
     const { id } = router.query
     const [details, setDetails] = useState(null)
     const [video, setVideo] = useState(null)
+    const [casts, setCasts] = useState([])
+    const [type, setType] = useState("trailer")
+    const [modalOpen, setModalOpen] = useState(false)
+
 
     // Function
+    const close = () => setModalOpen(false)
+    const open = (type) => {
+        setType(type)
+        setModalOpen(true)
+    }
+
     const getMovieDetails = useCallback(async () => {
         const result = await fetch(process.env.NEXT_PUBLIC_URL + `/${id}?api_key=${process.env.NEXT_PUBLIC_API_KEY}&language=en-US&page=1`)
         const data = await result.json()
@@ -40,22 +54,36 @@ const MovieDetails = () => {
         return () => clearTimeout(delayDebounceFn)
     }, [id])
 
+    const getMovieCast = useCallback(async () => {
+        const result = await fetch(process.env.NEXT_PUBLIC_URL + `/${id}/credits?api_key=${process.env.NEXT_PUBLIC_API_KEY}&language=en-US&page=1`)
+        const data = await result.json()
+
+        let delayDebounceFn = setTimeout(() => {
+            setCasts(data.cast)
+        }, 750)
+
+        return () => clearTimeout(delayDebounceFn)
+    }, [id])
+
     // Lifecycle
     useEffect(() => {
         if (id) {
             getMovieDetails()
             getMovieVideo()
+            getMovieCast()
         }
-    }, [id, getMovieDetails, getMovieVideo])
+    }, [id, getMovieDetails, getMovieVideo, getMovieCast])
 
     // Render
     return details
         ? (
             <>
+
                 <Head>
-                    <title>MovieDB - {details.title}</title>
+                    <title>Movieku | {details.title}</title>
                     <meta name="keyword" content={details.title} />
                 </Head>
+
                 <div className="pageContainer">
                     <div className={styles.root}>
                         <div className={styles.imgContainer}>
@@ -76,35 +104,89 @@ const MovieDetails = () => {
                             </h1>
                             <div className={styles.movieGenre}>
                                 {details.genres.map((genre, index) => (
-                                    <>
+                                    <div key={index}>
                                         <span>
                                             {genre.name}
                                         </span>
                                         {index === details.genres.length - 1 ? '' : <>&nbsp;&#9679;&nbsp;</>}
-                                    </>
+                                    </div>
                                 ))}
                             </div>
+                            <div className={styles.cast}>
+                                <h3>Main Cast</h3>
+                                <div className={styles.castWrapper}>
+                                    {casts.map((cast, index) => {
+                                        if (index < 6) return (
+                                            <div
+                                                key={cast.cast_id}
+                                                className={styles.castBox}
+                                            >
+                                                <Image
+                                                    width={80}
+                                                    height={80}
+                                                    className={styles.castImg}
+                                                    src={castURL + cast.profile_path}
+                                                />
+                                                <p>
+                                                    {cast.name}
+                                                </p>
+                                            </div>
+                                        )
+                                    })}
+                                </div>
+                            </div>
                             <div className={styles.synopsis}>
-                                <h3>Synopsis</h3>
+                                <h3>Overview</h3>
                                 <p>
                                     {details.overview}
                                 </p>
                             </div>
                             {video && (
-                                <div className={styles.video}>
-                                    <h3>Trailer</h3>
-                                    <iframe
-                                        allowFullScreen
-                                        className={styles.trailer}
-                                        title="YouTube video player"
-                                        src={`https://www.youtube.com/embed/${video[0].key}`}
-                                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                                    />
+                                <div className={styles.media}>
+                                    <h3>Media</h3>
+                                    <div className="d-flex">
+                                        <button
+                                            className="btn-main"
+                                            onClick={() => open("trailer")}
+                                        >
+                                            <BsPlayFill size={20} /> &nbsp; Play Trailer
+                                        </button>
+                                        <button
+                                            className="btn-main"
+                                            onClick={() => open("gallery")}
+                                        >
+                                            <BsImages size={20} /> &nbsp; Gallery
+                                        </button>
+                                    </div>
                                 </div>
                             )}
                         </div>
                     </div>
                 </div>
+
+                <AnimatePresence
+                    // Disable any initial animations on children.
+                    initial={false}
+                    // Only render one component at a time.
+                    exitBeforeEnter={true}
+                    // Fires when all exiting nodes have completed animating out.
+                    onExitComplete={() => null}
+                >
+                    {modalOpen && (
+                        <Modal
+                            modalOpen={modalOpen}
+                            handleClose={close}
+                            type={type}
+                            video={video}
+                            gallery={{
+                                posterURL: imgURL,
+                                poster: details.poster_path,
+                                backdropURL: backdropURL,
+                                backdrop: details.backdrop_path,
+                            }}
+                        />
+                    )}
+                </AnimatePresence>
             </>
         )
         : <Skeleton type="movie" />

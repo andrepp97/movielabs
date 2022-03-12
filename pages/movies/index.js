800aedcd023ce38aaa9b22372e7b69227e5f1b08
Andre Putera Pratama <andreputerap@gmail.com>
2022-03-12 23:45:31 +0700

    Create index.js

---
+++ pages/movies/index.js
@@ -0,0 +1,129 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import { useState, useEffect } from 'react'
+import { ImSearch } from 'react-icons/im'
+import { motion, AnimatePresence } from 'framer-motion'
+import { MovieCard, Skeleton } from '../../components'
+import styles from '../../styles/Home.module.css'
+
+const Movies = () => {
+    // State
+    const [text, setText] = useState("")
+    const [display, setDisplay] = useState("")
+    const [result, setResult] = useState(null)
+    const [loading, setLoading] = useState(false)
+
+    // Function
+    const searchMovies = async () => {
+        // Get the movies
+        setLoading(true)
+        const query = `&query=${text}`
+        const result = await fetch(process.env.NEXT_PUBLIC_SEARCH_URL + `?api_key=${process.env.NEXT_PUBLIC_API_KEY}&language=en-US&page=1&include_adult=false${query}`)
+        const result2 = await fetch(process.env.NEXT_PUBLIC_SEARCH_URL + `?api_key=${process.env.NEXT_PUBLIC_API_KEY}&language=en-US&page=2&include_adult=false${query}`)
+        const data = await result.json()
+        const data2 = await result2.json()
+
+        // Filtering the result
+        let temp = data.results.filter(movie => movie.poster_path && movie.release_date)
+        if (data2.results.length) {
+            let temp2 = data2.results.filter(movie => movie.poster_path && movie.release_date)
+            temp = temp.concat(temp2)
+        }
+
+        // Assign the filtered result to state
+        setResult(temp)
+        setDisplay(text)
+        setLoading(false)
+    }
+
+    const preventDefault = (e) => {
+        if (e.key === "Enter") {
+            e.preventDefault()
+        }
+    }
+
+    // Lifecycle
+    useEffect(() => {
+        if (text) {
+            let debounceFunction = setTimeout(async () => {
+                searchMovies()
+            }, 1000)
+
+            return () => clearTimeout(debounceFunction)
+        }
+    }, [text])
+
+    // Render
+    return (
+        <div className="pageContainer">
+
+            <Head>
+                <title>{display && display + ' - '}Movieku</title>
+            </Head>
+
+            <form className={styles.search}>
+                <ImSearch />
+                <input
+                    type="text"
+                    className={styles.searchInput}
+                    placeholder="Search for a movie . . ."
+                    onKeyDown={e => preventDefault(e)}
+                    onChange={e => setText(e.target.value)}
+                    value={text}
+                />
+            </form>
+
+            {(!result && loading === false) && (
+                <div className={styles.searchSvg}>
+                    <p>
+                        Type a word to start searching
+                    </p>
+                    <Image
+                        width={480}
+                        height={480}
+                        src="/search.svg"
+                        alt='Try "The Batman"'
+                    />
+                </div>
+            )}
+
+            {
+                result && loading === false
+                    ? (
+                        <p className={styles.resultText}>
+                            Showing <strong>{result.length}</strong> {result.length > 1 ? "results" : "result"} for "<strong>{display}</strong>"
+                        </p>
+                    )
+                    : null
+            }
+
+            <motion.div className={loading ? styles.loadingWrapper : styles.popular}>
+                <AnimatePresence>
+                    {
+                        loading
+                            ? <Skeleton type="slider" />
+                            : (
+                                result
+                                    ? result.length
+                                        ? result.map((movie, index) => (
+                                            <MovieCard
+                                                data={movie}
+                                                key={`${movie.id} - ${index}`}
+                                            />
+                                        ))
+                                        : (
+                                            <h3 className="movieYear">
+                                                There are no movies that matched your keyword
+                                            </h3>
+                                        )
+                                    : null
+                            )
+                    }
+                </AnimatePresence>
+            </motion.div>
+
+        </div>
+    );
+}
+
+export default Movies;

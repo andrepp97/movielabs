8812fd36eb99d3218f36afc316d601f55dc18e2e
Andre Putera Pratama <andreputerap@gmail.com>
2022-03-13 22:39:43 +0700

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,13 @@
 import Head from 'next/head'
 import Layout from '../layout/Layout'
+import NextNProgress from 'nextjs-progressbar'
 import '../styles/globals.css'
 import '../styles/Carousel.css'
 
 function MyApp({ Component, pageProps }) {
     return (
         <Layout>
+            <NextNProgress color="#f2f2f2" />
             <Head>
                 <title>Movieku</title>
                 <link rel="icon" href="/favicon.png" />

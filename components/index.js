6f19367e955f2275464db9960826c0472a608a26
Andre Putera Pratama <andreputerap@gmail.com>
2022-03-18 12:58:21 +0700

    Update index.js

--- components/index.js
+++ components/index.js
@@ -1,6 +1,7 @@
 export { default as Carousel } from './Carousel';
 export { default as MovieCard } from './MovieCard';
 export { default as MovieGenre } from './MovieGenre';
+export { default as MovieDetail } from './MovieDetail';
 export { default as MovieSlider } from './MovieSlider';
 export { default as Skeleton } from './Skeleton';
 export { default as Modal } from './Modal';

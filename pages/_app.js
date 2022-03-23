39ee5b48c8428449ee5728a22008d5dd4cdbe0a6
Andre Putera Pratama <andreputerap@gmail.com>
2022-03-23 15:24:18 +0700

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,11 @@
 import Head from 'next/head'
 import Layout from '../layout/Layout'
 import NextNProgress from 'nextjs-progressbar'
+import { AnimatePresence, motion } from 'framer-motion'
 import '../styles/globals.css'
 import '../styles/Carousel.css'
 
-function MyApp({ Component, pageProps }) {
+function MyApp({ Component, pageProps, router }) {
     return (
         <Layout>
             <NextNProgress color="#f2f2f2" stopDelayMs={250} />
@@ -13,7 +14,27 @@ function MyApp({ Component, pageProps }) {
                 <link rel="icon" href="/favicon.png" />
                 <meta name="keyword" content="Movie DB" />
             </Head>
-            <Component {...pageProps} />
+            <AnimatePresence>
+                <motion.div
+                    key={router.route}
+                    initial="pageInitial"
+                    animate="pageAnimate"
+                    exit="pageExit"
+                    variants={{
+                        pageInitial: {
+                            opacity: 0
+                        },
+                        pageAnimate: {
+                            opacity: 1
+                        },
+                        pageExit: {
+                            opacity: 0
+                        },
+                    }}
+                >
+                    <Component {...pageProps} />
+                </motion.div>
+            </AnimatePresence>
         </Layout>
     )
 }

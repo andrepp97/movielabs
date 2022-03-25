1811496e4b0a8aeb62235202d9ef0dd520732c93
Andre Putera Pratama <andreputerap@gmail.com>
2022-03-25 11:39:19 +0700

    Update MovieGenre.js

--- components/MovieGenre.js
+++ components/MovieGenre.js
@@ -1,51 +1,28 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react'
+import { motion } from 'framer-motion'
 import styles from '../styles/MovieGenre.module.css'
 
-const genre = [
-    {
-        id: 0,
-        name: 'All'
-    },
-    {
-        id: 28,
-        name: 'Action'
-    },
-    {
-        id: 12,
-        name: 'Adventure'
-    },
-    {
-        id: 16,
-        name: 'Animation'
-    },
-    {
-        id: 35,
-        name: 'Comedy'
-    },
-    {
-        id: 80,
-        name: 'Crime'
-    },
-    {
-        id: 14,
-        name: 'Fantasy'
-    },
-    {
-        id: 9648,
-        name: 'Mystery'
-    },
-    {
-        id: 878,
-        name: 'Sci-Fi'
-    },
-    {
-        id: 53,
-        name: 'Thriller'
-    },
-]
+const MovieGenre = ({ title, popular, setFiltered, genreList, activeGenre, setActiveGenre }) => {
+    // State & Ref
+    const genreRef = useRef()
+    const [width, setWidth] = useState(0)
+    const [genre, setGenre] = useState(null)
 
-const MovieGenre = ({ title, popular, setFiltered, activeGenre, setActiveGenre }) => {
     // Lifecycle
+    useEffect(() => {
+        if (genreList) {
+            let temp = [...genreList]
+            temp.unshift({ id: 0, name: "All" })
+            temp = temp.filter(item => item.id !== 10770)
+            temp.forEach((item, idx) => { if (item.name === "Science Fiction") temp[idx].name = "Sci-Fi" })
+            setGenre(temp)
+        }
+    }, [genreList])
+
+    useEffect(() => {
+        setWidth(genreRef.current.scrollWidth - genreRef.current.offsetWidth + 50)
+    }, [genre])
+
     useEffect(() => {
         if (popular) {
             if (activeGenre == 0) {
@@ -64,17 +41,24 @@ const MovieGenre = ({ title, popular, setFiltered, activeGenre, setActiveGenre }
             <p className="sectionTitle">
                 {title}
             </p>
-            <div className={styles.genreContainer}>
-                {genre.map((value, idx) => (
-                    <div
-                        key={idx}
-                        onClick={() => setActiveGenre(value.id)}
-                        className={activeGenre === value.id ? styles.genreActive : styles.genreItem}
-                        style={idx !== genre.length - 1 ? { marginRight: '12px' } : { marginright: 0 }}
-                    >
-                        {value.name}
-                    </div>
-                ))}
+            <div ref={genreRef} className={styles.genreContainer}>
+                <motion.div
+                    drag="x"
+                    whileDrag={{ pointerEvents: "none" }}
+                    dragConstraints={{ right: 0, left: -width }}
+                    className={styles.genreSlider}
+                >
+                    {genre && genre.map((value, idx) => (
+                        <div
+                            key={idx}
+                            onClick={() => setActiveGenre(value.id)}
+                            className={activeGenre === value.id ? styles.genreActive : styles.genreItem}
+                            style={idx !== genre.length - 1 ? { marginRight: '12px' } : { marginright: 0 }}
+                        >
+                            {value.name}
+                        </div>
+                    ))}
+                </motion.div>
             </div>
         </>
     );

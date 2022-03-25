477760aee8869240d9ce4b1c46780a84a8c9f7ed
Andre Putera Pratama <andreputerap@gmail.com>
2022-03-25 15:01:33 +0700

    Update Footer.js

--- layout/Footer.js
+++ layout/Footer.js
@@ -1,7 +1,39 @@
+import Image from "next/image"
+
+const socials = [
+    {
+        name: "Linkedin",
+        icon: "/icons8-linkedin.svg",
+        url: "https://www.linkedin.com/in/andreputera",
+    },
+    {
+        name: "Github",
+        icon: "/icons8-github.svg",
+        url: "https://github.com/andrepp97",
+    },
+]
+
 const Footer = () => {
     return (
         <footer>
-            &copy; Copyright 2022 <strong> | Andre Putera Pratama</strong>
+            &copy; Copyright 2022 - <strong>Andre Putera Pratama</strong>
+            <div className="social">
+                {socials.map(social => (
+                    <a
+                        key={social.name}
+                        href={social.url}
+                        target="_blank"
+                        rel="nofollow"
+                    >
+                        <Image
+                            width={30}
+                            height={30}
+                            alt={social.name}
+                            src={social.icon}
+                        />
+                    </a>
+                ))}
+            </div>
         </footer>
     );
 }
